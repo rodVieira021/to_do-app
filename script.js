fe67f5ec9381444a7f248013bdf9bd088c7ac993
rodVieira021 <rodvieira021@gmail.com>
2022-06-05 01:26:41 +0300

    done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const task = input.value;
+    let task = input.value;
 
     if (!task) {
       alert("Please fill out the task");
@@ -18,7 +18,6 @@ window.addEventListener("load", () => {
 
     const task_content_el = document.createElement("div");
     task_content_el.classList.add("content");
-    // task_content_el.innerText = task;
 
     task_el.appendChild(task_content_el);
 
@@ -30,6 +29,52 @@ window.addEventListener("load", () => {
 
     task_content_el.appendChild(task_input_el);
 
+    const task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    const task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerHTML = "Edit";
+
+    const task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerHTML = "Del";
+
+    const task_done_el = document.createElement('button')
+    task_done_el.classList.add('done')
+    task_done_el.innerHTML = 'done'
+
+    task_actions_el.appendChild(task_done_el)
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el);
+
     list_el.appendChild(task_el);
+
+    input.value = "";
+
+    task_edit_el.addEventListener("click", () => {
+      if (task_edit_el.innerText.toLowerCase() == "edit") {
+        task_input_el.removeAttribute("readonly");
+        task_input_el.focus();
+        task_edit_el.innerText = "Save";
+      } else {
+        task_input_el.setAttribute("readonly", "readonly");
+        task_edit_el.innerHTML = "Edit";
+        
+      }
+    });
+
+    task_done_el.addEventListener('click', ()=>{
+        task_input_el.style.textDecoration = 'line-through';
+        task_edit_el.remove()
+        task_done_el.remove()
+        
+    })
+
+    task_delete_el.addEventListener("click", () => {
+        list_el.removeChild(task_el)
+    });
   });
 });
